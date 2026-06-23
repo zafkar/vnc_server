333198df4b225d3328923ddd3f2db52b649e3a38
Zif <Zif>
2026-06-23 21:41:50 +0200

    added pixel format conversion tests and fix

--- src/protocol/mod.rs
+++ src/protocol/mod.rs
@@ -8,6 +8,9 @@ pub mod pixel_format;
 pub mod primitives;
 pub mod server_msg;
 
+#[cfg(test)]
+mod test;
+
 pub trait SendInto: Sized {
     #[allow(async_fn_in_trait)]
     async fn send<S: AsyncWrite + Unpin>(&self, stream: S) -> Result<()>;
