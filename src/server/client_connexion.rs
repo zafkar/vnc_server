ef6b05c336bf3819503935ab9327e58e21fc7a42
Zif <Zif>
2026-06-23 22:10:30 +0200

    Added a passthough to pixel_format conversion id same format

--- src/server/client_connexion.rs
+++ src/server/client_connexion.rs
@@ -20,6 +20,7 @@ use anyhow::Result;
 use tokio::{
     io::{AsyncRead, AsyncWrite},
     sync,
+    time::Instant,
 };
 use tracing::{debug, error, info, warn};
 
@@ -105,10 +106,14 @@ impl ClientConnexion {
                 ClientMessage::FramebufferUpdateRequest { incremental, rect } => {
                     debug!("Client asks for {rect:?}, incremental {incremental:?}");
                     if self.receive_screen_frame.has_changed()? || incremental == Flag::No {
+                        let start_time = Instant::now();
                         let data = self.receive_screen_frame.borrow().clone();
                         self.receive_screen_frame.mark_unchanged();
                         let dest_pixel_format_data = match &target_pixel_format {
-                            Some(dest_format) => self.pixel_format.convert_data_to_pixel_format(
+                            // Some(dest_format) if *dest_format == data.format => {
+                            //     data.get_src_rect(rect, self.height as usize)
+                            // }
+                            Some(dest_format) => data.format.convert_data_to_pixel_format(
                                 dest_format,
                                 &data.get_src_rect(rect, self.height as usize),
                             )?,
@@ -119,6 +124,7 @@ impl ClientConnexion {
                         )
                         .send(&mut stream)
                         .await?;
+                        info!("RF update_time : {:?}", start_time.elapsed());
                     } else {
                         ServerMessage::FramebufferUpdate(vec![])
                             .send(&mut stream)
