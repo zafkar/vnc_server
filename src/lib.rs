434fdb4f60a9838e8358132cf86d07118a6778ce
Zif <Zif>
2026-06-26 15:01:47 +0200

    dependencies cleanup

--- src/lib.rs
+++ src/lib.rs
@@ -4,3 +4,6 @@ pub mod config;
 pub mod input_controller;
 pub mod protocol;
 pub mod server;
+
+#[cfg(all(feature = "auth_provider_pam", not(target_os = "linux")))]
+compile_error!("The feature auth_provider_pam only works on linux");
